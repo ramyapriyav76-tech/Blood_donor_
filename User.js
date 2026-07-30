53d00020d5a45ca0c2a9b16f50602b52521cab12
RAMYAPRIYA <ramyapriya.v76@gmail.com>
2026-07-30 23:35:39 +0530

    User.js

---
+++ User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'donor', 'admin'],
+    default: 'user',
+  },
+  profilePic: {
+    type: String,
+    default: '',
+  },
+}, {
+  timestamps: true,
+});
+
+// Match entered password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
